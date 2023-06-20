a4ba717fbe978d639622fe2900124fba4483dd6c
Soumya323 <soumyaripan@gmail.com>
2023-06-21 00:02:02 +0530

    on button press fetch the result from minion api and update result text

--- app.js
+++ app.js
@@ -1,9 +1,31 @@
 var textarea = document.querySelector("#text-area");
 var button = document.querySelector("#translate-button");
-var resultText = document.querySelector("result-text");
+var resultText = document.querySelector("#result-text");
 
 button.addEventListener("click", translateClicked);
 
 function translateClicked() {
-    console.log("Clicked");
+    var serverURL = "https://api.funtranslations.com/translate/minion.json?";
+    var textQuery = "text=" + textarea.value;
+    var queryURL = serverURL + textQuery;
+
+    fetch(queryURL)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            updateResultUI(JSON.stringify(data));
+        })
+        .catch(error => {
+            console.error('Error', error);
+        });
+
+    // updateResultUI(" Hey kya bolti tuuu");
 }
+
+function updateResultUI(jsondata) {
+
+    const data = JSON.parse(jsondata);
+    result = data.contents.translated;
+    resultText.innerText = result;
+}
+
