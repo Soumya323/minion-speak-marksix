69df880970727401e2e727bcf9993308f0b98e0c
Soumya323 <soumyaripan@gmail.com>
2023-06-20 15:37:23 +0530

    added html, css and js files with basic page setup

---
+++ app.js
@@ -0,0 +1,9 @@
+var textarea = document.querySelector("#text-area");
+var button = document.querySelector("#translate-button");
+var resultText = document.querySelector("result-text");
+
+button.addEventListener("click", translateClicked);
+
+function translateClicked() {
+    console.log("Clicked");
+}
