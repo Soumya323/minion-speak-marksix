3bf2864b4bbf4dfe632853e786ed3c32ce9d69c2
Soumya323 <soumyaripan@gmail.com>
2023-06-22 06:54:52 +0530

    updated to show alert on empty input box

--- app.js
+++ app.js
@@ -5,23 +5,37 @@ var resultTextArea = document.querySelector("#result-text-area");
 button.addEventListener("click", translateClicked);
 
 function translateClicked() {
-    var serverURL = "https://api.funtranslations.com/translate/minion.json?";
-    var textQuery = "text=" + textarea.value;
-    var queryURL = serverURL + textQuery;
 
-    resultTextArea.value = "minion translating...  beee bop boop.... ";
+    if (!checkEmptyTextBox()) {
 
-    fetch(queryURL)
-        .then(response => response.json())
-        .then(data => {
-            console.log(data);
-            updateResultUI(JSON.stringify(data));
-        })
-        .catch(error => {
-            console.error('Error', error);
-        });
+        var serverURL = "https://api.funtranslations.com/translate/minion.json?";
+        var textQuery = "text=" + textarea.value;
+        var queryURL = serverURL + textQuery;
 
-    // TestResult(" Hey kya bolti tuuu");
+        resultTextArea.value = "minion translating...  beee bop boop.... ";
+
+        fetch(queryURL)
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
+                updateResultUI(JSON.stringify(data));
+            })
+            .catch(error => {
+                console.error('Error', error);
+            });
+
+        // TestResult("Hey kya bolti tuuu");
+    }
+    else {
+        alert("Please enter some text.");
+    }
+}
+
+function checkEmptyTextBox() {
+    if (textarea.value == "")
+        return true;
+    else
+        return false;
 }
 
 function updateResultUI(jsondata) {
